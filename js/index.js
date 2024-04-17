db9f747a498a257e805c94eb2b2b965bcb7112ae
Danil <tabak7771@gmail.com>
2024-04-17 17:55:09 +0300

    initial

---
+++ js/index.js
@@ -0,0 +1,103 @@
+function isTouchDevice() {
+    const maxTouchPoints = navigator.maxTouchPoints || 0;
+    const touchEvent = 'ontouchstart' in window;
+    const touchMediaQuery = window.matchMedia("(hover: none) and (pointer: coarse)").matches;
+
+    return maxTouchPoints > 0 || touchEvent || touchMediaQuery;
+}
+
+if (isTouchDevice()) {
+    document.body.classList.add('_touch');
+} else {
+    document.body.classList.add('_pc')
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuItemsWithSubmenu = document.querySelectorAll('.menu-list > li');
+
+    menuItemsWithSubmenu.forEach(item => {
+        const submenu = item.querySelector('.menu-sub-list');
+
+        item.addEventListener('click', function (event) {
+            if (submenu) {
+                const isVisible = submenu.style.display === 'block';
+                document.querySelectorAll('.menu-sub-list').forEach(sub => {
+                    sub.style.display = 'none';
+                });
+
+                submenu.style.display = isVisible ? 'none' : 'block';
+
+                event.stopPropagation();
+            }
+        });
+    });
+
+    const iconMenu = document.querySelector('.menu-icon');
+    if (iconMenu) {
+        const menuWrapper = document.querySelector('.menu-wrapper');
+        iconMenu.addEventListener("click", function (e) {
+            document.body.classList.toggle('_lock');
+            iconMenu.classList.toggle('_active');
+            menuWrapper.classList.toggle('_active');
+        })
+    }
+});
+
+/* Footer */
+const today = new Date().toLocaleDateString('en-US', { weekday: 'long' }).toLowerCase();
+const todayElement = document.getElementById(today);
+if (todayElement) {
+    todayElement.classList.add('today');
+}
+
+/* Sponsors slider */
+$('.sponsors-slider').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 2000,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    swipeToSlide: false,
+    centerMode: true,
+    variableWidth: true,
+    responsive: [
+        /* {
+            breakpoint: 1199,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 470,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        }, */
+    ],
+});
