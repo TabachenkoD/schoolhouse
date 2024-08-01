2677beee65c5f7bd3da80a04da7a0f730acc9d81
Danil <tabak7771@gmail.com>
2024-08-01 22:48:44 +0300

    test admin 2

---
+++ admin/assets/Classes-DNuHtHik.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BKr2Zhzr.js";const e=()=>s.jsx("div",{children:"test classes"});export{e as default};
--- admin/assets/Classes-Qb4nw0-7.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-BABda1oZ.js";const e=()=>s.jsx("div",{children:"test classes"});export{e as default};
