fcb0347d8414c817a038a15cf1e93bc247d020ab
Danil <tabak7771@gmail.com>
2024-09-25 15:40:06 +0300

    add login to admin

---
+++ admin/assets/Classes-B8rBl5DL.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BvV2htGL.js";const e=()=>s.jsx("div",{children:"test classes"});export{e as default};
---
+++ admin/assets/Classes-D0f4B3XY.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-syOInrvV.js";const e=()=>s.jsx("div",{children:"test classes"});export{e as default};
